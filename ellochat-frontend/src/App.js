aed88424cdffe46c49d5212de7da5b68b56a4c08
Fábrica Formiga - João Paulo <fabrica.formiga@basis.com.br>
2021-01-09 15:06:05 -0300

    Segundo commit, estruturaçao basica

--- ellochat-frontend/src/App.js
+++ ellochat-frontend/src/App.js
@@ -1,8 +1,15 @@
+import {Button} from 'primereact/button';
+import 'primereact/resources/themes/saga-orange/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import Routes from './routes';
 
 function App() {
   return (
-    <Routes/>
+    <div>
+      <Routes/>
+      <Button label="Save"></Button>
+    </div>
   );
 }
 
--- ellochat-frontend/src/routes.js
+++ ellochat-frontend/src/routes.js
@@ -5,14 +5,14 @@ export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={}/>
+                {/* <Route path="/" exact component={}/>
                 <Route path="/login" component={}/>
                 <Route path="/register" component={}/>
                 <Route path="/profile" component={}/>
                 <Route path="/chat/:id" component={}/>
                 <Route path="/chat/new" component={}/>
                 <Route path="/group/:id" component={}/>
-                <Route path="/group/new" component={}/>
+                <Route path="/group/new" component={}/> */}
             </Switch>
         </BrowserRouter>
     )
