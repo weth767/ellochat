132b38ecc192ec021b2d70de4b0cdeb1b2431171
leandropinheiroo <leandropinheiro98@gmail.com>
2021-02-02 19:58:17 -0300

    Criado metodo de recuperar Senha de usuario

--- ellochat-frontend/src/App.js
+++ ellochat-frontend/src/App.js
@@ -1,6 +1,7 @@
 import Routes from './routes';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Recover from './pages/Recover';
 import Home from './pages/Home';
 import './global.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Login}/>
         <Route path="/register" component={Register}/>
+        <Route path="/recover" component={Recover}/>
         <Route path="/home" component={Home}/>
       </Switch>
     </BrowserRouter>
--- ellochat-frontend/src/pages/Recover/index.js
+++ ellochat-frontend/src/pages/Recover/index.js
@@ -16,62 +16,51 @@ import './styles.css';
 
 
 
-export default function Login() {
+export default function Recover() {
 
     const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
 
     const [blocking, setBlocking] = useState(false);
 
-    const dispatch = useDispatch();
     
-    function handleLogin() {
+    function handleRecover() {
+        console.log(email);
         setBlocking(true);
         firebase.auth()
-        .signInWithEmailAndPassword(email,senha)
+        .sendPasswordResetEmail(email)
         .then(() => {
-            console.log("aasd");
-            dispatch({
-                type:'LOG_IN',
-                payload:{
-                    usuarioEmail:email
-                }
-            })
-            
-        }).catch(() => {
-            NotificationManager.warning("Erro ao realizar o login, verifique suas credenciais!", "Erro", 1000);
+            NotificationManager.success('Link Enviado para o E-mail!', 'Sucesso',1000);
+        }).catch((error) => {
+            console.log(error);
+            NotificationManager.warning('Verifique se o E-mail digitado é válido!', 'Erro',1000);
         }).finally(() => {
             setBlocking(false);
         });
+        
     }
 
     return (
         <BlockUi tag="div" blocking={blocking}>
-            <div className="login-content text-center">
+            <div className="recover-content text-center">
                 <form className="mx-auto col-sm-12 col-md-8 col-lg-5">
-                    <img src={Logo} className="login-logo" alt=""></img>
+                    <img src={Logo} className="recover-logo" alt=""></img>
                     <div className="card">
                         <div className="row">
                             <div className="col-md-12 d-flex align-items-center flex-column">    
-                                <h3 className="h3 mb-3 font-weight-bold text-blue"> Conectar-se </h3>
+                                <h3 className="h3 mb-3 font-weight-bold text-blue"> Recuperar Senha </h3>
                                 <div className="col-12 col-md-10 ">
-                                    <input type="email" className="form-control login-input text-blue" id="inputEmail" aria-describedby="emailHelp" placeholder="E-mail" 
+                                    <input type="email" className="form-control recover-input text-blue" id="inputEmail" aria-describedby="emailHelp" placeholder="E-mail" 
                                     onChange={e => setEmail(e.target.value)} />
                                 </div>
                                 <div className="col-12 col-md-10 ">
-                                    <input type="password" className="form-control login-input text-blue" id="inputPassowrd" placeholder="Senha"
-                                        onChange={e => setSenha(e.target.value)}/>
-                                </div>
-                                <div className="col-12 col-md-10 ">
-                                    <button type="button" className="btn btn-lg btn-login btn-block" onClick={handleLogin} >Entrar</button>
+                                    <button type="button" className="btn btn-lg btn-recover btn-block" onClick={handleRecover} >Recuperar Senha</button>
                                 </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-md-12 d-flex align-items-center flex-column">    
-                                <div className="col-md-10 d-flex justify-content-between">
-                                    <div><Link to ="/recover" className="mx-2 text-blue">Esqueci minha senha</Link></div>
-                                    <div><Link to ="/register" className="mx-2 text-blue">Cadastre-se agora</Link></div>
+                                <div className="col-md-10 d-flex justify-content-center">
+                                    <div><Link to ="/" className="mx-2 text-blue">Efetuar login</Link></div>
                                 </div>
                             </div>
                         </div>
