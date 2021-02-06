c1fb54b452cc88b0c0380ac82409a18c6cb46235
Fábrica Formiga - João Paulo <fabrica.formiga@basis.com.br>
2021-02-06 17:00:43 -0300

    Finalizado o cabeçalho e input do componente de chat

--- ellochat-frontend/src/components/ChatComponent/index.js
+++ ellochat-frontend/src/components/ChatComponent/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './styles.css';
-import Wallpaper from '../../assets/wallpaper.gif';
+import { MdAccountCircle, MdAttachFile, MdSend, MdMic }  from 'react-icons/md';
 
-export default function ChatComponent() {
+export default function ChatComponent(props) {
     return (
         <div className="chat">
-            <img src ={Wallpaper} alt="wallpaper"/>
+            <div className="chat-header">
+                {props.image ?
+                    <img src={props.image} alt="imagem do usuário" /> :
+                    <MdAccountCircle color="white" className="user-icon" />
+                }
+                <span>{props.contactName}</span>
+            </div>
+            <div className="chat-content">
+                
+            </div>
+            <div className="chat-input">
+                <div class="chat-input-attach">
+                    <label for="file-input">
+                        <MdAttachFile color="white" className="chat-input-icons"/>
+                    </label>
+                    <input id="file-input" type="file"/>
+                </div>
+                <div class="chat-input-wrapper">
+                    <input className="chat-input-input" placeholder="Digite uma mensagem"/>
+                </div>
+                <div className="chat-input-interactions">
+                    <button type="button" className="chat-input-buttons">
+                        <MdMic color="white" className="chat-input-icons"/>
+                    </button>
+                    <button type="button" className="chat-input-buttons">
+                        <MdSend color="white" className="chat-input-icons"/>
+                    </button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
