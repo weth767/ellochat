974ec93b6bb89e773b53689d62ec3003f3a7bb56
Fábrica Formiga - João Paulo <fabrica.formiga@basis.com.br>
2021-02-27 17:06:04 -0300

    Problema do blockui resolvido

--- ellochat-frontend/src/components/ContactList/index.js
+++ ellochat-frontend/src/components/ContactList/index.js
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
-import firebase from "../../config/firebase";
-import BlockUi from "react-block-ui";
 import "react-block-ui/style.css";
 import { useSelector } from 'react-redux';
 import { users } from '../../config/firebaseroutes';
 
 export default function ContactList({newChatCallback}) {
-    const database = firebase.database();
-    const [blocking, setBlocking] = useState(false);
     const [contacts, setContacts] = useState([]);
     const userEmail = useSelector(state => state.user.userEmail);
     const [dataLoaded, setDataLoaded] = useState(false);
 
     useEffect(() => {
         if (!dataLoaded) {
-            setBlocking(true);
             users.doc(userEmail).collection('contacts').get().then((docs) => {
                 let contactList = [];
                 docs.forEach(d => {
@@ -26,50 +21,48 @@ export default function ContactList({newChatCallback}) {
                 setContacts(contactList);
             })
         }
-    }, [blocking, database, userEmail, dataLoaded]);
+    }, [userEmail, dataLoaded]);
 
     return (
-        <BlockUi tag="div" blocking={blocking}>
-            <div className="modal" id="contactList" tabIndex="-1" role="dialog"
-                aria-labelledby="contactListTitle" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h3 className="h3 mb-3 font-weight-bold text-blue"> Contatos </h3>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="contacts">
-                            <ul>
-                                {contacts.map(contact => (
-                                    <li key={contact.email}>
-                                        <div className="contact-info" onClick={() => newChatCallback(contact)}>
-                                            <div className="card-body contact-info-content">
-                                                <div className="contact-text">
-                                                    <h2 className="card-title">
-                                                        {contact.username}
-                                                    </h2>
-                                                    <span className="card-text">
-                                                        {
-                                                            contact.status ? contact.status : 
-                                                            "Olá, comecei a usar o Ellochat"
-                                                        }
-                                                    </span>
-                                                </div>
+        <div className="modal" id="contactList" tabIndex="-1" role="dialog"
+            aria-labelledby="contactListTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h3 className="h3 mb-3 font-weight-bold text-blue"> Contatos </h3>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="contacts">
+                        <ul>
+                            {contacts.map(contact => (
+                                <li key={contact.email}>
+                                    <div className="contact-info" onClick={() => newChatCallback(contact)}>
+                                        <div className="card-body contact-info-content">
+                                            <div className="contact-text">
+                                                <h2 className="card-title">
+                                                    {contact.username}
+                                                </h2>
+                                                <span className="card-text">
+                                                    {
+                                                        contact.status ? contact.status : 
+                                                        "Olá, comecei a usar o Ellochat"
+                                                    }
+                                                </span>
                                             </div>
                                         </div>
-                                    </li>
+                                    </div>
+                                </li>
 
-                                ))}
-                            </ul>  
-                            <div className="modal-footer mt-3 ">
-                                <button type="button" className="btn btn-danger" data-dismiss="modal">Fechar</button>
-                            </div>
+                            ))}
+                        </ul>  
+                        <div className="modal-footer mt-3 ">
+                            <button type="button" className="btn btn-danger" data-dismiss="modal">Fechar</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </BlockUi>
+        </div>
     );
 }
